b2ad59e65b719191a1a4244291c0f27a8cd4fb9f
Thomas Gassmann <thomas@gassmann.dev>
2025-03-03 21:48:27 +0100

    Re-enable benchmarks
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- runner/benches/benchmarks/risc0.rs
+++ runner/benches/benchmarks/risc0.rs
@@ -4,11 +4,11 @@ use runner::types::{ProgramId, ProverId};
 
 fn risc0_programs(c: &mut Criterion) {
     // benchmark all programs *in the current state* against risc0
-    // add_benchmarks_for(ProgramId::Factorial, ProverId::Risc0, c);
-    // add_benchmarks_for(ProgramId::Keccak256, ProverId::Risc0, c);
+    add_benchmarks_for(ProgramId::Factorial, ProverId::Risc0, c);
+    add_benchmarks_for(ProgramId::Keccak256, ProverId::Risc0, c);
     add_benchmarks_for(ProgramId::LoopSum, ProverId::Risc0, c);
-    // add_benchmarks_for(ProgramId::RustTests, ProverId::Risc0, c);
-    // add_benchmarks_for(ProgramId::Sha256, ProverId::Risc0, c);
+    add_benchmarks_for(ProgramId::RustTests, ProverId::Risc0, c);
+    add_benchmarks_for(ProgramId::Sha256, ProverId::Risc0, c);
 }
 
 criterion_group!(risc0, risc0_programs);
--- runner/benches/benchmarks/sp1.rs
+++ runner/benches/benchmarks/sp1.rs
@@ -4,11 +4,11 @@ use runner::types::{ProgramId, ProverId};
 
 fn sp1_programs(c: &mut Criterion) {
     // benchmark all programs *in the current state* against sp1
-    // add_benchmarks_for(ProgramId::Factorial, ProverId::SP1, c);
-    // add_benchmarks_for(ProgramId::Keccak256, ProverId::SP1, c);
+    add_benchmarks_for(ProgramId::Factorial, ProverId::SP1, c);
+    add_benchmarks_for(ProgramId::Keccak256, ProverId::SP1, c);
     add_benchmarks_for(ProgramId::LoopSum, ProverId::SP1, c);
-    // add_benchmarks_for(ProgramId::RustTests, ProverId::SP1, c);
-    // add_benchmarks_for(ProgramId::Sha256, ProverId::SP1, c);
+    add_benchmarks_for(ProgramId::RustTests, ProverId::SP1, c);
+    add_benchmarks_for(ProgramId::Sha256, ProverId::SP1, c);
 }
 
 criterion_group!(sp1, sp1_programs);
