9366640e60348fa8900387ac1f690a73d7ae5eb7
Thomas Gassmann <thomas@gassmann.dev>
2025-03-09 01:18:55 +0100

    Add more programs from a16z zkvm-benchmarks
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

---
+++ programs/sha3/src/main.rs
@@ -0,0 +1,26 @@
+#![no_main]
+
+use sha3::{Digest, Keccak256};
+extern crate alloc;
+
+#[cfg(feature = "risc0")]
+risc0_zkvm::guest::entry!(main);
+
+#[cfg(feature = "sp1")]
+sp1_zkvm::entrypoint!(main);
+
+pub fn main() {
+    #[cfg(feature = "sp1")]
+    let input: Vec<u8> = sp1_zkvm::io::read();
+    #[cfg(feature = "risc0")]
+    let input: Vec<u8> = risc0_zkvm::guest::env::read();
+
+    let mut hasher = Keccak256::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+
+    #[cfg(feature = "sp1")]
+    sp1_zkvm::io::commit::<[u8; 32]>(&result.into());
+    #[cfg(feature = "risc0")]
+    risc0_zkvm::guest::env::commit::<[u8; 32]>(&result.into());
+}
