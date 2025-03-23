3d75401f53b8cd3e4d988fdc9db5fc1c278aa22d
Thomas Gassmann <thomas@gassmann.dev>
2025-03-23 19:53:12 +0100

    Reduce input size
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- programs/polybench-2mm/src/main.rs
+++ programs/polybench-2mm/src/main.rs
@@ -13,7 +13,8 @@ fn bench_and_print<const NI: usize, const NJ: usize, const NK: usize, const NL: 
 }
 
 fn main() {
-    bench_and_print::<200, 225, 250, 275>();
+    bench_and_print::<8, 9, 10, 11>();
+    // bench_and_print::<200, 225, 250, 275>();
     // bench_and_print::<400, 450, 500, 550>();
     // bench_and_print::<800, 900, 1000, 1100>();
 }
