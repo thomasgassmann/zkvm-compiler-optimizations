67d85020ccaca2e96188fad6afbed22e9628a93f
Thomas Gassmann <thomas@gassmann.dev>
2025-03-31 17:11:26 +0200

    Use flat sampling for prove measurements
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -143,6 +143,9 @@ fn run_criterion(args: CriterionArgs) {
                 let group_name = format!("{}-{}-{}", program, prover, measurement);
                 let mut group = c.benchmark_group(&group_name);
                 group.sample_size(10);
+                if *measurement == MeasurementType::Prove {
+                    group.sampling_mode(criterion::SamplingMode::Flat);
+                }
 
                 for profile in profiles.iter() {
                     add_benchmarks_for(&program, &prover, &mut group, &measurement, &profile);
