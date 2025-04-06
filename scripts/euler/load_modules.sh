93f3286225a0c03d9c7c956cbd75b9b841ea9700
Thomas Gassmann <thomas@gassmann.dev>
2025-04-07 01:19:10 +0200

    Fix module loading
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- scripts/euler/load_modules.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-module load stack/2024-06 openssl/3.1.3-zhfub4o cuda/12.1.1 gperftools/2.13
--- scripts/euler/run.sh
+++ scripts/euler/run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+sbatch <<EOT
+#!/bin/bash
 
-sbatch --output=out.txt --mem-per-cpu=32GB --time=5-00:00:00 --gpus=1 --gres=gpumem:24g --wrap="./zkbench.sh $@"
+#SBATCH --output=out.txt
+#SBATCH --error=out.txt
+#SBATCH --mem-per-cpu=32GB
+#SBATCH --time=5-00:00:00
+#SBATCH --gpus=1
+#SBATCH --gres=gpumem:24g
+
+module load stack/2024-06 openssl/3.1.3-zhfub4o cuda/12.1.1 gperftools/2.13
+./zkbench.sh $@
+
+exit 0
+EOT
