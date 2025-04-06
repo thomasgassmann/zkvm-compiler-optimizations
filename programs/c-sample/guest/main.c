22b3a4c3a85d84b87589728e794ff465962d949f
Thomas Gassmann <thomas@gassmann.dev>
2025-04-06 20:51:25 +0200

    Use printf_compat for c programs
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- programs/c-sample/guest/main.c
+++ programs/c-sample/guest/main.c
@@ -1,10 +1,13 @@
 extern void printsomething(int a);
 extern int add(int a, int b);
+extern int printf(const char *fmt, ...);
 
 int cmain()
 {
   int b = 42; // unused;
   int a = 21;
   printsomething(a);
+  printf("Hello, world!\n");
+  printf("Hello, world! formatted: %d\n", a);
   return add(a, a);
 }
--- programs/c-sample/host/src/main.rs
+++ programs/c-sample/host/src/main.rs
@@ -1,11 +1,15 @@
 #![no_main]
 #![allow(unused)]
+#![feature(c_variadic)]
 
 #[cfg(feature = "risc0")]
 risc0_zkvm::guest::entry!(main);
 
 #[cfg(feature = "sp1")]
 sp1_zkvm::entrypoint!(main);
+
+use std::os::raw::{c_char, c_int};
+use printf_compat::{format, output};
 
 #[no_mangle]
 pub extern "C" fn add(a: i32, b: i32) -> i32 {
@@ -15,6 +19,14 @@ pub extern "C" fn add(a: i32, b: i32) -> i32 {
 #[no_mangle]
 pub extern "C" fn printsomething(a: i32) -> () {
     println!("Hello from Rust! {}", a);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn printf(str: *const c_char, mut args: ...) -> c_int {
+    let mut s = String::new();
+    let bytes_written = format(str, args.as_va_list(), output::fmt_write(&mut s));
+    print!("{}", s);
+    bytes_written
 }
 
 #[link(name = "c-sample", kind = "static")]
