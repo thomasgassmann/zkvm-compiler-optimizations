112b7dbd9ef4a0cdb5766413cf3cd2dda3ce94c0
Thomas Gassmann <thomas@gassmann.dev>
2025-04-16 15:46:16 +0200

    Update euler scripts
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- scripts/euler/run80.sh
+++ scripts/euler/run80.sh
@@ -2,12 +2,11 @@
 sbatch <<EOT
 #!/bin/bash
 
-#SBATCH --output=out40.txt
-#SBATCH --error=out40.txt
+#SBATCH --output=out80.txt
+#SBATCH --error=out80.txt
 #SBATCH --mem-per-cpu=32GB
-#SBATCH --time=15-00:00:00
-#SBATCH --gpus=1
-#SBATCH --gres=gpumem:40g
+#SBATCH --time=9-00:00:00
+#SBATCH --gpus=a100_80gb:1
 
 module load stack/2024-06 openssl/3.1.3-zhfub4o cuda/12.1.1 gperftools/2.13
 ./zkbench.sh $@
