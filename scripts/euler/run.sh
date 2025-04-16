15e70501172333ff6f7c4441b12904aaa696efa5
Thomas Gassmann <thomas@gassmann.dev>
2025-04-16 22:48:08 +0200

    Rework euler scripts
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- scripts/euler/run.sh
+++ scripts/euler/run.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+OUT="${OUT:-out.txt}"
+TIMED="${TIMED:-5}"
+GPUMEM="${GPUMEM:-24g}"
+
 sbatch <<EOT
 #!/bin/bash
 
-#SBATCH --output=out.txt
-#SBATCH --error=out.txt
-#SBATCH --time=9-00:00:00
+#SBATCH --output=${OUT}
+#SBATCH --error=${OUT}
+#SBATCH --time=${TIMED}-00:00:00
 #SBATCH --gpus=1
-#SBATCH --gres=gpumem:24g
+#SBATCH --gres=gpumem:${GPUMEM}
 
 module load stack/2024-06 openssl/3.1.3-zhfub4o cuda/12.1.1 gperftools/2.13
-./zkbench.sh $@
+./zkbench.sh \$@
 
 exit 0
 EOT
