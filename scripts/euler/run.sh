1c463be3d9d32cfe5d6a5b2abbd9307a2bc5809d
Thomas Gassmann <thomas@gassmann.dev>
2025-04-18 15:27:56 +0200

    Allow specifying gpu model for running on euler
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- scripts/euler/run.sh
+++ scripts/euler/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 OUT="${OUT:-out.txt}"
 TIMED="${TIMED:-5}"
-GPUMEM="${GPUMEM:-24g}"
+GPUMEM="${GPUMEM:-1}"
+# models currently working with sp1  : rtx_3090, a100-pci-40gb, a100_80gb
+# models currently working with risc0: all tested so far
+GPUS="${GPUS:-a100}"
 
 sbatch <<EOT
 #!/bin/bash
@@ -10,7 +13,7 @@ sbatch <<EOT
 #SBATCH --output=${OUT}
 #SBATCH --error=${OUT}
 #SBATCH --time=${TIMED}-00:00:00
-#SBATCH --gpus=1
+#SBATCH --gpus=${GPUS}
 #SBATCH --gres=gpumem:${GPUMEM}
 
 module load stack/2024-06 openssl/3.1.3-zhfub4o cuda/12.1.1 gperftools/2.13
