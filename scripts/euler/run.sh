3ccd9d3895ef4dbd2f48b024125b9951229814a8
Thomas Gassmann <thomas@gassmann.dev>
2025-04-24 15:52:33 +0200

    Print nvidia-smi before running
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- scripts/euler/run.sh
+++ scripts/euler/run.sh
@@ -17,6 +17,8 @@ sbatch <<EOT
 #SBATCH --gpus=${GPUS}
 #SBATCH --gres=gpumem:${GPUMEM}
 
+nvidia-smi
+
 module load stack/2024-06 openssl/3.1.3-zhfub4o cuda/12.1.1 gperftools/2.13
 ./zkbench.sh $@
 
