9eee9d64339af55fb3374f6d739a0a765580fe88
Thomas Gassmann <thomas@gassmann.dev>
2025-04-27 20:40:33 +0200

    Fix opt-by-program plot
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- zkbench/plot/opt_by_program.py
+++ zkbench/plot/opt_by_program.py
@@ -22,24 +22,29 @@ def plot_opt_by_program(dir: str, profile: str, zkvm: str | None):
         try:
             exec_values = []
             prove_values = []
-            for zkvm in get_zkvms() if not zkvm else [zkvm]:
-                prove = get_point_estimate_mean_ms(dir, program, zkvm, profile, "prove")
-                exec = get_point_estimate_mean_ms(dir, program, zkvm, profile, "exec")
+            for current_zkvm in get_zkvms() if not zkvm else [zkvm]:
+                prove = get_point_estimate_mean_ms(
+                    dir, program, current_zkvm, profile, "prove"
+                )
+                exec = get_point_estimate_mean_ms(
+                    dir, program, current_zkvm, profile, "exec"
+                )
                 prove_baseline = get_point_estimate_mean_ms(
-                    dir, program, zkvm, BASELINE, "prove"
+                    dir, program, current_zkvm, BASELINE, "prove"
                 )
                 exec_baseline = get_point_estimate_mean_ms(
-                    dir, program, zkvm, BASELINE, "exec"
+                    dir, program, current_zkvm, BASELINE, "exec"
                 )
 
                 exec_values.append((exec_baseline - exec) / exec_baseline)
                 prove_values.append((prove_baseline - prove) / prove_baseline)
 
+            print(len(exec_values), len(prove_values))
             relative_improvements_exec.append(exec_values)
             relative_improvements_prove.append(prove_values)
             plotted_programs.add(program)
         except FileNotFoundError:
-            logging.warning(f"Data for {program}-{zkvm}-{profile} not found")
+            logging.warning(f"Data for {program}-{current_zkvm}-{profile} not found")
 
     if len(relative_improvements_exec[0]) == 1:
         # if we only have one value, no need to plot boxplot
