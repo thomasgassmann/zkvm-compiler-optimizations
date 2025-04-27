3ac34f15875d9c60a1d05980d484c1120060d6c3
Thomas Gassmann <thomas@gassmann.dev>
2025-04-27 23:38:34 +0200

    Plot baselines for genetic runs
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- zkbench/tune/plot/genetic.py
+++ zkbench/tune/plot/genetic.py
@@ -1,16 +1,24 @@
-import json
-
 from matplotlib import pyplot as plt
+
+from zkbench.tune.genetic import Genetic
+from zkbench.tune.plot.common import read_genetic_stats
 
 
 def plot_genetic(stats: str): 
-    stats = json.loads(open(stats).read())
+    stats: Genetic = read_genetic_stats(stats)
 
-    plt.plot(stats['values'], marker='o', linestyle='-')
+    plt.plot(stats.values, marker="o", linestyle="-")
 
+    colors = plt.cm.tab10.colors
+    for i, baseline in enumerate(stats.baselines):
+        value = stats.baselines[baseline]
+        metric_sum = sum([v.metric for v in value])
+        plt.axhline(y=metric_sum, label=baseline, color=colors[i % len(colors)])
+
+    plt.legend()
     plt.xlabel('Iteration')
-    plt.ylabel("Metric value")
-    plt.title('Metric over Iterations')
+    plt.ylabel(f"Metric value ({stats.metric})")
+    plt.title(f"Metric over Iterations ({stats.mode_name})")
 
     plt.grid()
     plt.show()
