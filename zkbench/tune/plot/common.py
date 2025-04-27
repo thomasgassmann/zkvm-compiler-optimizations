1a846db41a18279a6a522901fb6ef2d0eeb56518
Thomas Gassmann <thomas@gassmann.dev>
2025-04-27 23:38:34 +0200

    Plot baselines for genetic runs
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- zkbench/tune/plot/common.py
+++ zkbench/tune/plot/common.py
@@ -1,8 +1,14 @@
 import json
 from dacite import from_dict
 from zkbench.tune.exhaustive import Exhaustive
+from zkbench.tune.genetic import Genetic
 
 
 def read_exhaustive_stats(stats: str):
     stats = json.loads(open(stats).read())
     return from_dict(Exhaustive, stats)
+
+
+def read_genetic_stats(stats: str):
+    stats = json.loads(open(stats).read())
+    return from_dict(Genetic, stats)
