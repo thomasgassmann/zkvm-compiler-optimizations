e7ff74e65885c4172b1d04871b9fe3cdd2806e04
Thomas Gassmann <thomas@gassmann.dev>
2025-05-18 00:00:22 +0200

    Remove unused scripts
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- scripts/euler/chmod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-chmod +x ./scripts/euler/run.sh
-chmod +x ./scripts/euler/run_with_config.sh
-chmod +x ./scripts/euler/run_with_config.py
-chmod +x ./zkbench.sh
-chmod +x ./scripts/euler/run_interactive.sh
-chmod +x ./scripts/euler/build.sh
-chmod +x ./scripts/euler/run_interactive_gpu.sh
