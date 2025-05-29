3fe24225316811d5eea8d8da482a2f47175b054b
Thomas Gassmann <thomas@gassmann.dev>
2025-05-29 15:32:27 +0200

    Add loop-sum
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- programs/loop-sum/src/main.rs
+++ programs/loop-sum/src/main.rs
@@ -6,15 +6,6 @@ risc0_zkvm::guest::entry!(main);
 #[cfg(feature = "sp1")]
 sp1_zkvm::entrypoint!(main);
 
-#[inline(never)]
-fn sum_array(arr: &Vec<i32>) -> i32 {
-    let mut sum = 0;
-    for &val in arr.iter() {
-        sum += val;
-    }
-    sum
-}
-
 pub fn main() {
     #[cfg(feature = "risc0")]
     let data: Vec<i32> = risc0_zkvm::guest::env::read();
