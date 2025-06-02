2f004c11279160f9add5308df86b80ee03ec23c0
Thomas Gassmann <thomas@gassmann.dev>
2025-06-02 17:22:39 +0200

    Fix erroneous entry in gitignore
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

---
+++ programs/factorial/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn factorial(num: u128) -> u128 {
+    (1..=num).product()
+}
+
+#[no_mangle]
+pub extern "C" fn main_core(n: u32) {
+    for _i in 0..n {
+        let _ = factorial(20);
+    }
+}
