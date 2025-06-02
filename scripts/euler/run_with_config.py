115e4cf900a5f5f5129f3025633961b25eb39965
Thomas Gassmann <thomas@gassmann.dev>
2025-06-02 22:58:08 +0200

    Update run config
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- scripts/euler/run_with_config.py
+++ scripts/euler/run_with_config.py
@@ -65,6 +65,10 @@ config = {
         "sp1": "GPUS=a100_80gb:1 GPUMEM=24g",
         "risc0": "GPUS=quadro_rtx_6000:1 GPUMEM=24g",
     },
+    "polybench-syr2k": {
+        "sp1": "GPUS=a100_80gb:1 GPUMEM=24g",
+        "risc0": "GPUS=quadro_rtx_6000:1 GPUMEM=24g",
+    },
     "default_sp1": "GPUS=rtx_3090:1 GPUMEM=24g",
     "default_risc0": "GPUS=quadro_rtx_6000:1 GPUMEM=24g",
 }
