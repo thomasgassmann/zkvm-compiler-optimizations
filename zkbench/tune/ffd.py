719904468d998090ef7bc29d9d1e9773a3774a21
Thomas Gassmann <thomas@gassmann.dev>
2025-06-15 18:10:58 +0200

    Ensure we always get all results
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- zkbench/tune/ffd.py
+++ zkbench/tune/ffd.py
@@ -5,10 +5,11 @@ import json
 import logging
 import os
 
-from zkbench.tune.runner import TuneRunner
+from zkbench.tune.runner import BuildResult, TuneRunner
 from zkbench.tune.common import (
     BIN_OUT_FFD,
     EvalResult,
+    MetricValue,
     ProfileConfig,
     TuneConfig,
     build_pass_list,
@@ -101,11 +102,12 @@ def run_tune_ffd(
         scgu = ("single_codegen_unit" in active) if config.tune_codegen_units else config.default_single_codegen_unit
         pp = ("prepopulate_passes" in active) if config.tune_prepopulate_passes else config.default_prepopulate_passes
 
-        REPEATS = 5
         # we cannot get any info about ordering anyways
         # some sequences of passes seem to cause a SEGFAULT in
         # the rust compiler
-        for _ in range(REPEATS):
+        remaining = None
+        eval_result = EvalResult(False, values=[])
+        while remaining is None or len(remaining) > 0:
             profile = ProfileConfig(
                 name=f"ffd_row_{idx}",
                 lto="fat" if "lto" in active else "off",
@@ -115,27 +117,79 @@ def run_tune_ffd(
                 prepopulate_passes=pp,
             )
 
-            logging.info("Row %d/%d: %s", idx + 1, len(design), profile)
-            res = asyncio.get_event_loop().run_until_complete(
-                runner.run_build(programs, zkvms, profile)
+            logging.info(
+                "Row %d/%d: %s - %s", idx + 1, len(design), profile.get_hash(), profile
             )
-            if any([not r.success for r in res]):
+            if remaining is None:
+                res = asyncio.get_event_loop().run_until_complete(
+                    runner.run_build(programs, zkvms, profile)
+                )
+            else:
+                remaining_zkvms = set([b.zkvm for b in remaining])
+                res = []
+                for current_zkvm in remaining_zkvms:
+                    current_programs = set(
+                        b.program for b in remaining if b.zkvm == current_zkvm
+                    )
+                    res.extend(
+                        asyncio.get_event_loop().run_until_complete(
+                            runner.run_build(
+                                list(current_programs), [current_zkvm], profile
+                            )
+                        )
+                    )
+
+            successful = [r for r in res if r.success]
+            metric_values = asyncio.get_event_loop().run_until_complete(
+                asyncio.gather(
+                    *[
+                        runner.eval_metric(
+                            metric,
+                            c.zkvm,
+                            c.program,
+                            runner.get_out_path(profile, c.zkvm, c.program),
+                            profile,
+                        )
+                        for c in successful
+                    ],
+                    return_exceptions=True,
+                )
+            )
+            new_eval_res = EvalResult(
+                False,
+                values=[
+                    val
+                    for val in metric_values
+                    if isinstance(val, MetricValue)
+                    and not val.timeout
+                    and not val.metric == -1
+                ],
+            )
+            eval_result = eval_result.merge(new_eval_res)
+
+            def get_eval(p: str, z: str) -> EvalResult:
+                relevant = [
+                    v for v in eval_result.values if v.program == p and v.zkvm == z
+                ]
+                if (
+                    len(relevant) == 1
+                    and not relevant[0].timeout
+                    and relevant[0].metric != -1
+                ):
+                    return relevant[0]
+                return None
+
+            remaining = [
+                BuildResult(p, z, False, "tmp")
+                for p in programs
+                for z in zkvms
+                if get_eval(p, z) is None
+            ]
+            if len(remaining) > 0:
                 logging.error("Build failed for config %s", profile)
                 logging.info("Trying to shuffle passes and retry")
                 random.shuffle(active_passes)
-                continue
-            break
-
-        if all([not r.success for r in res]):
-            logging.error("Build failed for config %s", profile)
-            results.append(FFDResult(idx, active_passes, active, profile, True, None))
-            flush()
-            continue
-
-        successful = [r for r in res if r.success]
-        eval_result = runner.eval_all(successful, profile)
-        build_error = any([not r.success for r in res])
         results.append(
-            FFDResult(idx, active_passes, active, profile, build_error, eval_result)
+            FFDResult(idx, active_passes, active, profile, False, eval_result)
         )
         flush()
