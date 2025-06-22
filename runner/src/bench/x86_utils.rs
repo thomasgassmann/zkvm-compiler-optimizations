c171909c0c2ed63957e525fd41603058f75ca4f1
Thomas Gassmann <thomas@gassmann.dev>
2025-06-22 17:05:57 +0200

    Try using iter_batched
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- runner/src/bench/x86_utils.rs
+++ runner/src/bench/x86_utils.rs
@@ -69,7 +69,7 @@ pub fn exec_x86_prepare<'a>(
     lib: &'a Library,
     program: &ProgramId,
     input_override: &Option<String>,
-) -> Box<dyn FnOnce() + 'static> {
+) -> (Box<dyn FnMut() -> Box<dyn std::any::Any>>, Box<dyn FnMut(Box<dyn std::any::Any>)>) {
     macro_rules! load_main_core_fn {
         ($fn_ty:ty) => {{
             let main_core_symbol: Symbol<$fn_ty> = unsafe {
@@ -84,164 +84,176 @@ pub fn exec_x86_prepare<'a>(
         ProgramId::Bigmem => {
             let inp = get_bigmem_input();
             let main_core_fn: MainCoreBigMem = load_main_core_fn!(MainCoreBigMem);
-            Box::new(move || unsafe {
-                main_core_fn(inp);
-            })
+            (
+                Box::new(move || Box::new(inp)),
+                Box::new(move |inp| unsafe {
+                    let inp = *inp.downcast::<u32>().expect("Invalid input type");
+                    main_core_fn(inp);
+                }),
+            )
         }
         ProgramId::EcdsaVerify => {
             let main_core_fn: MainCoreEcdsaVerify = load_main_core_fn!(MainCoreEcdsaVerify);
             let (encoded_verifying_key, message, signature) = rand_ecdsa_signature();
-            Box::new(move || unsafe {
-                main_core_fn(encoded_verifying_key, message, signature);
-            })
+            (
+                Box::new(move || {
+                    Box::new((encoded_verifying_key, message.clone(), signature))
+                }),
+                Box::new(move |inp| unsafe {
+                    let (encoded_verifying_key, message, signature) =
+                        *inp.downcast::<(k256::EncodedPoint, Vec<u8>, k256::ecdsa::Signature)>()
+                            .expect("Invalid input type");
+                    main_core_fn(encoded_verifying_key, message, signature);
+                }),
+            )
         }
-        ProgramId::EddsaVerify => {
-            let main_core_fn: MainCoreEddsaVerify = load_main_core_fn!(MainCoreEddsaVerify);
-            let mut input = Vec::new();
-            for _ in 0..get_eddsa_times() {
-                input.push(rand_eddsa_signature());
-            }
-            Box::new(move || unsafe {
-                main_core_fn(input);
-            })
-        }
-        ProgramId::Factorial => {
-            let main_core_fn: MainCoreFactorial = load_main_core_fn!(MainCoreFactorial);
-            let inp = get_factorial_input();
-            Box::new(move || unsafe {
-                main_core_fn(inp);
-            })
-        }
-        ProgramId::Fibonacci => {
-            let main_core_fn: MainCoreFibonacci = load_main_core_fn!(MainCoreFibonacci);
-            let inp = get_fibonacci_input();
-            Box::new(move || unsafe {
-                main_core_fn(inp);
-            })
-        }
-        ProgramId::Keccak256 => {
-            let main_core_fn: MainCoreKeccak256 = load_main_core_fn!(MainCoreKeccak256);
-            let inp = get_keccak256_input();
-            Box::new(move || unsafe {
-                main_core_fn(inp);
-            })
-        }
-        ProgramId::LoopSum => {
-            let main_core_fn: MainCoreLoopSum = load_main_core_fn!(MainCoreLoopSum);
-            let inp = get_loop_sum_input();
-            Box::new(move || unsafe {
-                main_core_fn(inp);
-            })
-        }
-        ProgramId::Merkle => {
-            let main_core_fn: MainCoreMerkle = load_main_core_fn!(MainCoreMerkle);
-            let (strings, range) = get_merkle_input();
-            Box::new(move || unsafe {
-                main_core_fn(strings, range);
-            })
-        }
-        ProgramId::Polybench2mm
-        | ProgramId::Polybench3mm
-        | ProgramId::PolybenchAdi
-        | ProgramId::PolybenchAtax
-        | ProgramId::PolybenchBicg
-        | ProgramId::PolybenchCholesky
-        | ProgramId::PolybenchCorrelation
-        | ProgramId::PolybenchCovariance
-        | ProgramId::PolybenchDeriche
-        | ProgramId::PolybenchDoitgen
-        | ProgramId::PolybenchDurbin
-        | ProgramId::PolybenchFdtd2d
-        | ProgramId::PolybenchFloydWarshall
-        | ProgramId::PolybenchGemm
-        | ProgramId::PolybenchGemver
-        | ProgramId::PolybenchGesummv
-        | ProgramId::PolybenchGramschmidt
-        | ProgramId::PolybenchHeat3d
-        | ProgramId::PolybenchJacobi1d
-        | ProgramId::PolybenchJacobi2d
-        | ProgramId::PolybenchLu
-        | ProgramId::PolybenchLudcmp
-        | ProgramId::PolybenchMvt
-        | ProgramId::PolybenchNussinov
-        | ProgramId::PolybenchSeidel2d
-        | ProgramId::PolybenchSymm
-        | ProgramId::PolybenchSyr2k
-        | ProgramId::PolybenchSyrk
-        | ProgramId::PolybenchTrisolv
-        | ProgramId::PolybenchTrmm
-        | ProgramId::NpbBt
-        | ProgramId::NpbCg
-        | ProgramId::NpbEp
-        | ProgramId::NpbFt
-        | ProgramId::NpbIs
-        | ProgramId::NpbLu
-        | ProgramId::NpbMg
-        | ProgramId::NpbSp
-        | ProgramId::Sha256
-        | ProgramId::Spec605 => {
-            let main_core_fn: MainCore = load_main_core_fn!(MainCore);
-            Box::new(move || unsafe {
-                main_core_fn();
-            })
-        }
-        ProgramId::RegexMatch => {
-            let main_core_fn: MainCoreRegexMatch = load_main_core_fn!(MainCoreRegexMatch);
-            let (regex, text) = get_regex_match_input();
-            Box::new(move || unsafe {
-                main_core_fn(regex, text);
-            })
-        }
-        ProgramId::Rsp => {
-            let main_core_fn: MainCoreRsp = load_main_core_fn!(MainCoreRsp);
-            let input = load_rsp_input(input_override);
-            Box::new(move || unsafe {
-                main_core_fn(&input);
-            })
-        }
-        ProgramId::Sha2Bench | ProgramId::Sha3Bench => {
-            let main_core_fn: MainCoreShaBench = load_main_core_fn!(MainCoreShaBench);
-            let input = get_sha_bench_input();
-            Box::new(move || unsafe {
-                main_core_fn(input);
-            })
-        }
-        ProgramId::Sha2Chain | ProgramId::Sha3Chain => {
-            let main_core_fn: MainCoreShaChain = load_main_core_fn!(MainCoreShaChain);
-            let (input, num_iters) = get_sha_chain_input();
-            Box::new(move || unsafe {
-                main_core_fn(input, num_iters);
-            })
-        }
-        ProgramId::Spec619 => {
-            let main_core_fn: MainCoreSpec619 = load_main_core_fn!(MainCoreSpec619);
-            let (it, action, sim_type) = get_spec619_input();
-            Box::new(move || unsafe {
-                main_core_fn(it, action, sim_type);
-            })
-        }
-        ProgramId::Spec631 => {
-            let main_core_fn: MainCoreSpec631 = load_main_core_fn!(MainCoreSpec631);
-            let str = include_str!("../../../inputs/spec-631/in.txt");
-            let input = str.to_string();
-            Box::new(move || unsafe {
-                main_core_fn(input);
-            })
-        }
-        ProgramId::Tailcall => {
-            let main_core_fn: MainCoreTailcall = load_main_core_fn!(MainCoreTailcall);
-            let (n, r) = get_tailcall_input();
-            Box::new(move || unsafe {
-                main_core_fn(n, r);
-            })
-        }
-        ProgramId::ZkvmMnist => {
-            let main_core_fn: MainCoreZkvmMnist = load_main_core_fn!(MainCoreZkvmMnist);
-            let (training_data, test_data) = load_mnist();
-            Box::new(move || unsafe {
-                main_core_fn(training_data, test_data);
-            })
-        }
+        // ProgramId::EddsaVerify => {
+        //     let main_core_fn: MainCoreEddsaVerify = load_main_core_fn!(MainCoreEddsaVerify);
+        //     let mut input = Vec::new();
+        //     for _ in 0..get_eddsa_times() {
+        //         input.push(rand_eddsa_signature());
+        //     }
+        //     Box::new(move || unsafe {
+        //         main_core_fn(input.clone());
+        //     })
+        // }
+        // ProgramId::Factorial => {
+        //     let main_core_fn: MainCoreFactorial = load_main_core_fn!(MainCoreFactorial);
+        //     let inp = get_factorial_input();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(inp);
+        //     })
+        // }
+        // ProgramId::Fibonacci => {
+        //     let main_core_fn: MainCoreFibonacci = load_main_core_fn!(MainCoreFibonacci);
+        //     let inp = get_fibonacci_input();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(inp);
+        //     })
+        // }
+        // ProgramId::Keccak256 => {
+        //     let main_core_fn: MainCoreKeccak256 = load_main_core_fn!(MainCoreKeccak256);
+        //     let inp = get_keccak256_input();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(inp);
+        //     })
+        // }
+        // ProgramId::LoopSum => {
+        //     let main_core_fn: MainCoreLoopSum = load_main_core_fn!(MainCoreLoopSum);
+        //     let inp = get_loop_sum_input();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(inp);
+        //     })
+        // }
+        // ProgramId::Merkle => {
+        //     let main_core_fn: MainCoreMerkle = load_main_core_fn!(MainCoreMerkle);
+        //     let (strings, range) = get_merkle_input();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(strings, range);
+        //     })
+        // }
+        // ProgramId::Polybench2mm
+        // | ProgramId::Polybench3mm
+        // | ProgramId::PolybenchAdi
+        // | ProgramId::PolybenchAtax
+        // | ProgramId::PolybenchBicg
+        // | ProgramId::PolybenchCholesky
+        // | ProgramId::PolybenchCorrelation
+        // | ProgramId::PolybenchCovariance
+        // | ProgramId::PolybenchDeriche
+        // | ProgramId::PolybenchDoitgen
+        // | ProgramId::PolybenchDurbin
+        // | ProgramId::PolybenchFdtd2d
+        // | ProgramId::PolybenchFloydWarshall
+        // | ProgramId::PolybenchGemm
+        // | ProgramId::PolybenchGemver
+        // | ProgramId::PolybenchGesummv
+        // | ProgramId::PolybenchGramschmidt
+        // | ProgramId::PolybenchHeat3d
+        // | ProgramId::PolybenchJacobi1d
+        // | ProgramId::PolybenchJacobi2d
+        // | ProgramId::PolybenchLu
+        // | ProgramId::PolybenchLudcmp
+        // | ProgramId::PolybenchMvt
+        // | ProgramId::PolybenchNussinov
+        // | ProgramId::PolybenchSeidel2d
+        // | ProgramId::PolybenchSymm
+        // | ProgramId::PolybenchSyr2k
+        // | ProgramId::PolybenchSyrk
+        // | ProgramId::PolybenchTrisolv
+        // | ProgramId::PolybenchTrmm
+        // | ProgramId::NpbBt
+        // | ProgramId::NpbCg
+        // | ProgramId::NpbEp
+        // | ProgramId::NpbFt
+        // | ProgramId::NpbIs
+        // | ProgramId::NpbLu
+        // | ProgramId::NpbMg
+        // | ProgramId::NpbSp
+        // | ProgramId::Sha256
+        // | ProgramId::Spec605 => {
+        //     let main_core_fn: MainCore = load_main_core_fn!(MainCore);
+        //     Box::new(move || unsafe {
+        //         main_core_fn();
+        //     })
+        // }
+        // ProgramId::RegexMatch => {
+        //     let main_core_fn: MainCoreRegexMatch = load_main_core_fn!(MainCoreRegexMatch);
+        //     let (regex, text) = get_regex_match_input();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(regex, text);
+        //     })
+        // }
+        // ProgramId::Rsp => {
+        //     let main_core_fn: MainCoreRsp = load_main_core_fn!(MainCoreRsp);
+        //     let input = load_rsp_input(input_override);
+        //     Box::new(move || unsafe {
+        //         main_core_fn(&input);
+        //     })
+        // }
+        // ProgramId::Sha2Bench | ProgramId::Sha3Bench => {
+        //     let main_core_fn: MainCoreShaBench = load_main_core_fn!(MainCoreShaBench);
+        //     let input = get_sha_bench_input();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(input);
+        //     })
+        // }
+        // ProgramId::Sha2Chain | ProgramId::Sha3Chain => {
+        //     let main_core_fn: MainCoreShaChain = load_main_core_fn!(MainCoreShaChain);
+        //     let (input, num_iters) = get_sha_chain_input();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(input, num_iters);
+        //     })
+        // }
+        // ProgramId::Spec619 => {
+        //     let main_core_fn: MainCoreSpec619 = load_main_core_fn!(MainCoreSpec619);
+        //     let (it, action, sim_type) = get_spec619_input();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(it, action, sim_type);
+        //     })
+        // }
+        // ProgramId::Spec631 => {
+        //     let main_core_fn: MainCoreSpec631 = load_main_core_fn!(MainCoreSpec631);
+        //     let str = include_str!("../../../inputs/spec-631/in.txt");
+        //     let input = str.to_string();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(input);
+        //     })
+        // }
+        // ProgramId::Tailcall => {
+        //     let main_core_fn: MainCoreTailcall = load_main_core_fn!(MainCoreTailcall);
+        //     let (n, r) = get_tailcall_input();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(n, r);
+        //     })
+        // }
+        // ProgramId::ZkvmMnist => {
+        //     let main_core_fn: MainCoreZkvmMnist = load_main_core_fn!(MainCoreZkvmMnist);
+        //     let (training_data, test_data) = load_mnist();
+        //     Box::new(move || unsafe {
+        //         main_core_fn(training_data, test_data);
+        //     })
+        // }
         _ => panic!("Unsupported program for x86 execution: {:?}", program),
     }
 }
