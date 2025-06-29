741dd153e69736230a15dc6d24695f6d33c712e8
Thomas Gassmann <thomas@gassmann.dev>
2025-06-30 01:23:36 +0200

    Add block_box where appropriate
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- programs/factorial/src/lib.rs
+++ programs/factorial/src/lib.rs
@@ -5,6 +5,7 @@ pub fn factorial(num: u128) -> u128 {
 #[no_mangle]
 pub extern "C" fn main_core(n: u32) {
     for _i in 0..n {
-        let _ = factorial(20);
+        let res = factorial(20);
+        core::hint::black_box(res);
     }
 }
